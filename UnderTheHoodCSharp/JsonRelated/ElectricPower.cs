24cea09381dee9e98baebdd5f3a62d1d1dd485e4
Willy <mibhpwosiay@gmail.com>
2018-06-08 10:56:30 +0800

    first commit

---
+++ UnderTheHoodCSharp/JsonRelated/ElectricPower.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace UnderTheHoodCSharp.JsonRelated
+{
+    public class ElectricPower
+    {
+        [JsonProperty(PropertyName = "phase", Required = Required.Always)]
+        public Phase Phase { get; set; }
+    }
+
+    /***
+    public enum Phase
+    {
+        L1,
+        L2,
+        L3,
+        N,
+        L1-N, //Syntax Error '-' is not invalid
+        L2-N, //Syntax Error '-' is not invalid
+        L3-N, //Syntax Error '-' is not invalid
+        L1-L2, //Syntax Error '-' is not invalid
+        L2-L3, //Syntax Error '-' is not invalid
+        L3-L1 //Syntax Error '-' is not invalid
+    }
+
+    ***/
+
+    public enum Phase
+    {
+        L1,
+        L2,
+        L3,
+        N,
+        [EnumMember(Value = "L1-N")]
+        L1N,
+        [EnumMember(Value = "L2-N")]
+        L2N,
+        [EnumMember(Value = "L3-N")]
+        L3N,
+        [EnumMember(Value = "L1-L2")]
+        L1L2,
+        [EnumMember(Value = "L2-L3")]
+        L2L3,
+        [EnumMember(Value = "L3-L1")]
+        L3L1
+    }
+}
---
+++ UnderTheHoodCSharp/JsonRelated/Example.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace UnderTheHoodCSharp.JsonRelated
+{
+    public class Example
+    {
+        public Example()
+        {
+            ElectricPower power = new ElectricPower()
+            {
+                Phase = Phase.L1L2
+            };
+
+            JsonSerializerSettings settings = new JsonSerializerSettings()
+            {
+                //ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Converters = new List<JsonConverter> { new StringEnumConverter { CamelCaseText = false } }, //Specified CamelCaseText StringConverter
+                NullValueHandling = NullValueHandling.Ignore
+            };
+            
+            Console.WriteLine(JsonConvert.SerializeObject(power, settings));
+        }
+    }
+}
